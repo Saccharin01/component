a9ea3d6e5a4b438f00d3a625a67ef23ea20b57dd
Saccharin01 <chousik01@gmail.com>
2024-06-11 21:02:02 +0900

    debug UX

--- app3.js
+++ app3.js
@@ -1,16 +1,44 @@
 const http = require("http");
 let container = [];
 let server = http.createServer((req, res) => {
-  container.push(req.url);
-  console.log(container);
-  if (req.method === "GET") {
-    if (container.includes(req.url) && req.url === "/") {
-      const htmlTemplate = require("./module.HTMLtemplate");
-      res.write(htmlTemplate());
-      res.end();
-    }
-    if (container.includes(req.url)) {
-      console.log(req.url);
+  if (!container.includes(req.url)) {
+    container.push(req.url);
+  } else {
+    console.log(container);
+    if (req.method === "GET") {
+      if (container.includes(req.url) && req.url === "/") {
+        const htmlTemplate = require("./module.HTMLtemplate");
+        res.write(htmlTemplate());
+        res.end();
+      }
+      if (container.includes(req.url) && req.url.split(".")[1] === "js") {
+        const fs = require("fs");
+        fs.readdir("./js", (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            fs.readFile(
+              `${req.url.split(".")[1]}/${data}`,
+              "utf-8",
+              (err, data) => {
+                if (err) {
+                  res.writeHead(500, { "content-Type": "text/plain" });
+                  res.write(`<h1>Server Error</h1>`);
+                  res.end();
+                  console.log(err);
+                } else {
+                  res.writeHead(200, {
+                    "content-Type": "Application/javaScript",
+                  });
+                  res.write(data);
+                  res.end();
+                }
+              }
+            );
+          }
+        });
+      } else {
+      }
     }
   }
 });
