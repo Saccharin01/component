e01e21e67b7f3324004607e69798ca074231b988
Saccharin01 <chousik01@gmail.com>
2024-06-11 17:46:08 +0900

    checkPoint

--- app2.js
+++ app2.js
@@ -5,10 +5,24 @@ const fs = require("fs");
 let container = [];
 let server = http.createServer((req, res) => {
   if (req.method === "GET") {
-    if (!container.includes(req.url)) {
-      container.push(req.url);
-    } else {
+    if (container.includes(req.url)) {
+      console.log(container.indexOf(req.rul));
       console.log("tracking");
+    } else {
+      container.push(req.url);
+      console.log(container);
+    }
+    if (req.url === container[container.indexOf(req.url)]) {
+      fs.readFile(`${}`,'utf-8',(err,data)=>{
+        if(err){
+          console.error(`this Error Occur : ${err}`)
+        } else {
+          res.writeHead(200, {"content-Type":""})
+          res.end();
+        }
+      })
+      console.log("tracking this part");
+      res.end();
     }
   }
 });
