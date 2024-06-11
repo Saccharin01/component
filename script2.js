05d87809ee6aabf2753c79f8bb16c2856d1632f2
Saccharin01 <chousik01@gmail.com>
2024-06-11 10:48:19 +0900

    capsulize funtion

--- script2.js
+++ script2.js
@@ -1,10 +1,11 @@
-const a = () => {
+const a = (name, age, job) => {
   return {
-    name: "",
-    age: 0,
-    job: "",
+    name: name,
+    age: age,
+    job: job,
   };
 };
 
-console.log(a());
-const b = a();
+// console.log(a());
+const b = a("saccharin01", 30, "Dev1");
+console.log(b);
