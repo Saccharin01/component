e7ab9561a34644db0e9dfdda74993892940e5e12
Saccharin01 <chousik01@gmail.com>
2024-06-11 17:27:57 +0900

    JavaScript : Calling module

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { names as baseData } from "./baseData.js";
-
+import { tagComponent as asd } from "./tagComponent.js";
+import { app as assambly } from "./assambly.js";
+import { render } from "./render.js";
 console.log(baseData);
+console.log(asd);
+console.log(assambly);
+console.log(render);
+
+const root = document.getElementById("root");
+
+document.addEventListener("DOMContentLoaded", () => {
+  render();
+});
