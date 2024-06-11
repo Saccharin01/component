497c6f50bb6f1d6c6450fe49b01b41283f5d0aa1
Saccharin01 <chousik01@gmail.com>
2024-06-11 17:27:18 +0900

    module inject : assambly.js

---
+++ assambly.js
@@ -0,0 +1,10 @@
+export const app = () => {
+  let i = names.map((ele) => {
+    if (ele === "조우식") {
+      return tagComponent("h1", ele);
+    } else {
+      return tagComponent("h3", ele);
+    }
+  });
+  return `${i.join("")}`;
+};
