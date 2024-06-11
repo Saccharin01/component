dab5f4c60991f076e29f39e46725a54233f02f53
Saccharin01 <chousik01@gmail.com>
2024-06-11 10:55:32 +0900

    object static/Dynamic

--- script2.js
+++ script2.js
@@ -8,4 +8,6 @@ const a = (name, age, job) => {
 
 // console.log(a());
 const b = a("saccharin01", 30, "Dev1");
-console.log(b);
+console.log("첫번째 :", b);
+b.address = "대전";
+console.log("두 번째 : ", b);
